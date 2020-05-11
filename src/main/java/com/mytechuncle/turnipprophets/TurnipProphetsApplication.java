34c8f11516723496c763ff7d4e21538f34beea8b
Ryan Nichols <rybread@gmail.com>
2020-05-11 13:05:22 -0400

    initial commit of a spring boot app

---
+++ src/main/java/com/mytechuncle/turnipprophets/TurnipProphetsApplication.java
@@ -0,0 +1,13 @@
+package com.mytechuncle.turnipprophets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnipProphetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnipProphetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytechuncle/turnipprophets/TurnipProphetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytechuncle.turnipprophets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnipProphetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
